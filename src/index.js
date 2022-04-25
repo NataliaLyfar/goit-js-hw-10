ef9f288cb01a318a3ff04895d9892bd0ffeae173
Nataliia Lyfar <lifarnatala@gmail.com>
2022-04-25 21:14:24 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,33 +2,53 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 
-import countryCardTpl from './country-card.hbs'
+import countryCardTpl from './country-card.hbs';
+import {fetchCountries} from './js/fetchCountries.js';
+import getRefs from './js/get-refs.js';
+
 const DEBOUNCE_DELAY = 300;
 
-const refs = {
-    searchInput: document.querySelector('#search-box'),
-    countryList: document.querySelector('.country-list'),
-    countryInfo: document.querySelector('.country-info')
+const refs = getRefs();
+
+const createCountryList = countries => {
+    return countries.map(el => {
+        return `<li><div class="country-title-box">
+        <img class="country-flag" width="25" 
+        src='${el.flags.svg}'/>
+        <h2 class='country-title'>${el.name.official}</h2>
+        </div></li>`
+    }).join('');
 }
-// const onSearch = e =>{
-//     e.preventDefault;
+const renderCountryCard = (countries) => {
+    const countriesQuentity = countries.length;
+    if(countriesQuentity > 10){
+        return Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")}
+    else if(countriesQuentity > 1 && countriesQuentity <= 10){
+        return refs.countryList.insertAdjacentHTML('beforeend', createCountryList(countries))} 
+    else if(countriesQuentity === 1){
+        refs.countryList.remove();
+        const markup = countryCardTpl(countries[0]);
+        return refs.countryInfo.innerHTML = markup;}
+        }
+const onFetchError = Error => {
+    Notiflix.Notify.failure('Oops! There is no country with that name!');
+    refs.countryInfo.remove();
+    return refs.countryList.remove();
+}
+const clearInput = e => {
+    const currentData = e.currentTarget.value;
+    if(currentData === ''){refs.countryInfo.remove();}
+}
+const onSearch = e => {
+    e.preventDefault;
+    const inputData = e.target.value.trim();
 
-// }
-// refs.searchInput.addEventListener('input', onSearch);
+    fetchCountries(inputData)
+    .then(renderCountryCard)
+    .catch(onFetchError);
 
-// const fetchCountries = (name) => {
+    clearInput();
+}
+refs.searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-// }
-fetch('https://restcountries.com/v3.1/name/peru')
-.then(response =>{
-   return response.json();
-}).then((country)=>{
-    console.log(country);
-    const markup = countryCardTpl(country[0]);
-    console.log(markup);
-    refs.countryInfo.innerHTML = markup;
-}).catch(error => {
-    return Notiflix.Notify.failure('Oops! There is no country with that name!');
-});
 
-// refs.countryList.insertAdjacentHTML("beforebegin", ``);
