099289fa1c27dfee9ed7a7e2cca9f1b427c6828b
Nataliia Lyfar <lifarnatala@gmail.com>
2022-05-12 18:39:21 +0300

    added styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import Notiflix from 'notiflix';
+import {Notify} from 'notiflix';
 import debounce from 'lodash.debounce';
 
 import {fetchCountries} from './js/fetchCountries.js';
@@ -8,39 +8,38 @@ import countryListTpl from './template/country-list.hbs';
 import countryCardTpl from './template/country-card.hbs';
 
 const DEBOUNCE_DELAY = 300;
-
-const createCountryList = countries => countries.map(country => countryListTpl(country)).join('');
-const renderCountryCard = (countries) => {
-    const countriesQuantity = countries.length;
-    if(countriesQuantity > 10){
-        refs.countryInfo.innerHTML = '';
-        refs.countryList.innerHTML = '';
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-    }
-    else if(countriesQuantity > 1 && countriesQuantity <= 10){
-        refs.countryInfo.innerHTML = '';
-        refs.countryList.innerHTML  = createCountryList(countries);
-    } 
-    else if(countriesQuantity === 1){
-        refs.countryList.innerHTML = '';
-        refs.countryInfo.innerHTML = countryCardTpl(countries[0]);
-    }
-        };
-const onFetchError = () => {
-    Notiflix.Notify.failure('Oops! There is no country with that name!');
+const clearMarkup = () => {
     refs.countryInfo.innerHTML = '';
     refs.countryList.innerHTML = '';
 };
-const clearInput = () => refs.countryInfo.innerHTML = '';
+const createCountryList = countries => countries.map(country => countryListTpl(country)).join('');
+const renderCountryCard = countries => {
+    const countriesQuantity = countries.length;
+    clearMarkup();
+    if(countriesQuantity > 10){
+        Notify.warning("Too many matches found. Please enter a more specific name.")
+    }
+    else if(countriesQuantity > 1 && countriesQuantity <= 10){
+        Notify.info(`Hooray! We found ${countriesQuantity} countries.`);
+        refs.countryList.innerHTML  = createCountryList(countries);
+    } 
+    else if(countriesQuantity === 1){
+        Notify.success(`This is exactly what you were looking for!`);
+        refs.countryInfo.innerHTML = countryCardTpl(countries[0]);
+    }
+    };
+const onFetchError = () => {
+    Notify.failure('Oops! There is no country with that name!');
+    clearMarkup();
+};
 const onSearch = e => {
     e.preventDefault;
-   
    if(e.target.value){
     fetchCountries(e.target.value.trim())
     .then(renderCountryCard)
     .catch(onFetchError);
-}
-    clearInput();
+    }
+    clearMarkup();
 };
 refs.searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -8,6 +8,6 @@ export const fetchCountries = (name) => {
         }
         return response.json();
       });
-}
+};
 
 
