47ce7b6b7da5b03b5a6981447f2c36f5d16f2859
Nataliia Lyfar <lifarnatala@gmail.com>
2022-05-14 21:25:52 +0300

    дштл екн

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ const clearMarkup = () => {
     refs.countryInfo.innerHTML = '';
     refs.countryList.innerHTML = '';
 };
-const createCountryList = countries => countries.map(country => countryListTpl(country)).join('');
+const createCountryList = countries => countryListTpl(countries);
+const createCountryMarkup = country => countryCardTpl(country);
+// const generateCountry = country => refs.countryInfo.innerHTML = createCountryMarkup(country);
+// const onClick = (name) => fetchCountries(name).then(generateCountry)
 const renderCountryCard = countries => {
     const countriesQuantity = countries.length;
     clearMarkup();
@@ -22,16 +25,25 @@ const renderCountryCard = countries => {
     else if(countriesQuantity > 1 && countriesQuantity <= 10){
         Notify.info(`Hooray! We found ${countriesQuantity} countries.`);
         refs.countryList.innerHTML  = createCountryList(countries);
+        // console.log(refs.countryList);
+        // const countryLink = document.querySelector('.country-link');
+        // const countryName = document.querySelector('.country-title');
+        // console.log(countryLink);
+        
+        // const name = countryName.textContent;
+        // countryLink.addEventListener('click', onClick);
+        // console.log(name);
     } 
     else if(countriesQuantity === 1){
         Notify.success(`This is exactly what you were looking for!`);
-        refs.countryInfo.innerHTML = countryCardTpl(countries[0]);
+        refs.countryInfo.innerHTML = createCountryMarkup(countries);
     }
     };
 const onFetchError = () => {
     Notify.failure('Oops! There is no country with that name!');
     clearMarkup();
 };
+
 const onSearch = e => {
     e.preventDefault;
    if(e.target.value){
@@ -41,6 +53,15 @@ const onSearch = e => {
     }
     clearMarkup();
 };
+
 refs.searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+
+
+
+
+
+
+
 
 
--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -1,5 +1,5 @@
 export const refs = {
         searchInput: document.querySelector('#search-box'),
         countryList: document.querySelector('.country-list'),
-        countryInfo: document.querySelector('.country-info')
+        countryInfo: document.querySelector('.country-info'),
 };
